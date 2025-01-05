28eb2ca84be50a0ab2bd1c768b06cd28c7e9fab0
Takafumi Fukai <takyafumin@gmail.com>
2025-01-05 12:00:09 +0900

    APIGatewayのイベント型を指定し、受信したイベントのパラメータ処理を修正。エラーログを改善。

--- lambdas/sender/src/index.ts
+++ lambdas/sender/src/index.ts
@@ -10,7 +10,8 @@ const QUEUE_URL = process.env.QUEUE_URL || '';
  * @param event
  * @returns
  */
-export const handler: Handler = async (event) => {
+// apigatewayのイベント型を指定
+export const handler: Handler<APIGatewayProxyEvent, APIGatewayProxyResult> = async (event) => {
 
   if (!QUEUE_URL) {
     console.log('Error: QUEUE_URL is not defined');
@@ -22,13 +23,14 @@ export const handler: Handler = async (event) => {
 
   // パラメータを出力
   console.log('Received event:', JSON.stringify(event.body, null, 2));
-  console.log('input1:', JSON.stringify(event.input1, null, 2));
-  console.log('input2:', JSON.stringify(event.input2, null, 2));
+  const body = JSON.parse(event.body || '{}');
+  console.log('input1:', JSON.stringify(body.input1, null, 2));
+  console.log('input2:', JSON.stringify(body.input2, null, 2));
   console.log('QueueUrl:', QUEUE_URL);
 
   const messageBody = {
-    input1: event.input1,
-    input2: event.input2,
+    input1: body.input1,
+    input2: body.input2,
   }
 
   const params = {
@@ -43,7 +45,7 @@ export const handler: Handler = async (event) => {
       body: JSON.stringify({ message: 'Message sent to SQS' }),
     };
   } catch (error) {
-    console.log('Error:', error);
+    console.error('Error:', error);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: 'Failed to send message to SQS', details: (error as any).message }),
