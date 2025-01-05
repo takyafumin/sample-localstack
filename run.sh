a4a8c7ebbf92490e7a33520c97cc4ebefa333872
Takafumi Fukai <takyafumin@gmail.com>
2025-01-05 21:02:29 +0900

    #3, package.jsonとrun.shの修正: 不要なS3アップロードスクリプトを削除し、CloudFormationスタック名を定数化

--- run.sh
+++ run.sh
@@ -4,12 +4,8 @@ ROOT_DIR=$(pwd)
 # S3バケット名を取得
 S3_BUCKET_NAME=sample-aws-sqs-lambda-functions-template
 
-# ----------------------------------------
-# S3バケット名を取得する関数
-# ----------------------------------------
-get_s3_bucket_name() {
-  aws cloudformation describe-stacks --stack-name S3BucketCFnStack --query "Stacks[0].Outputs[?OutputKey=='S3BucketNameCFn'].OutputValue" --output text
-}
+# ParentStackの定義
+PARENT_STACK_NAME="SampleAwsSqsLambdaFunctionsStack"
 
 # ----------------------------------------
 # CloudFormationスタックを構築する関数
@@ -29,29 +25,29 @@ deploy_stacks() {
 
   # Lambda関数をS3にアップロード
   echo "Uploading Lambda functions to S3..."
-  npm run upload-sender
-  npm run upload-receiver
+  aws s3 cp lambdas/sender/index.zip s3://$S3_BUCKET_NAME/sender/index.zip
+  aws s3 cp lambdas/receiver/index.zip s3://$S3_BUCKET_NAME/receiver/index.zip
 
   # テンプレートをS3にアップロード
   echo "Uploading templates to S3..."
-  aws s3 cp cloudformation/template.yml s3://${S3_BUCKET_NAME}/cloudformation/template.yml
-  aws s3 cp cloudformation/resources/sqs.yml s3://${S3_BUCKET_NAME}/cloudformation/resources/sqs.yml
-  aws s3 cp cloudformation/resources/lambda.yml s3://${S3_BUCKET_NAME}/cloudformation/resources/lambda.yml
-  aws s3 cp cloudformation/resources/api-gateway.yml s3://${S3_BUCKET_NAME}/cloudformation/resources/api-gateway.yml
+  aws s3 cp cloudformation/template.yml s3://$S3_BUCKET_NAME/cloudformation/template.yml
+  aws s3 cp cloudformation/resources/sqs.yml s3://$S3_BUCKET_NAME/cloudformation/resources/sqs.yml
+  aws s3 cp cloudformation/resources/lambda.yml s3://$S3_BUCKET_NAME/cloudformation/resources/lambda.yml
+  aws s3 cp cloudformation/resources/api-gateway.yml s3://$S3_BUCKET_NAME/cloudformation/resources/api-gateway.yml
 
   # スタックをデプロイ
   echo "Deploying parent stack..."
-  aws cloudformation deploy --template-file ${ROOT_DIR}/cloudformation/template.yml \
-    --stack-name ParentStack \
+  aws cloudformation deploy --template-file $ROOT_DIR/cloudformation/template.yml \
+    --stack-name $PARENT_STACK_NAME \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides \
-        LambdaS3BucketName=${S3_BUCKET_NAME} \
+        LambdaS3BucketName=$S3_BUCKET_NAME \
         LambdaS3BucketKeySender=sender/index.zip \
         LambdaS3BucketKeyReceiver=receiver/index.zip
 
-  API_GATEWAY_URL=$(aws cloudformation describe-stacks --stack-name ParentStack --query "Stacks[0].Outputs[?OutputKey=='ApiGatewayUrlProd'].OutputValue" --output text)
+  API_GATEWAY_URL=$(aws cloudformation describe-stacks --stack-name $PARENT_STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='ApiGatewayUrlProd'].OutputValue" --output text)
   echo "API Gateway URL (Prod): $API_GATEWAY_URL"
-  API_GATEWAY_URL_DEV=$(aws cloudformation describe-stacks --stack-name ParentStack --query "Stacks[0].Outputs[?OutputKey=='ApiGatewayUrlDev'].OutputValue" --output text)
+  API_GATEWAY_URL_DEV=$(aws cloudformation describe-stacks --stack-name $PARENT_STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='ApiGatewayUrlDev'].OutputValue" --output text)
   echo "API Gateway URL (Dev): $API_GATEWAY_URL_DEV"
 
   echo "Deployment complete."
@@ -64,12 +60,11 @@ delete_stacks() {
   echo "Deleting CloudFormation stacks..."
 
   # ParentStackを削除
-  aws cloudformation delete-stack --stack-name ParentStack
-  aws cloudformation wait stack-delete-complete --stack-name ParentStack
+  aws cloudformation delete-stack --stack-name $PARENT_STACK_NAME
+  aws cloudformation wait stack-delete-complete --stack-name $PARENT_STACK_NAME
 
   # S3バケットの中身を削除
   echo "Deleting S3 bucket contents..."
-  S3_BUCKET_NAME=$(get_s3_bucket_name)
   aws s3 rm s3://$S3_BUCKET_NAME --recursive
 
   # S3バケットのスタックを削除
@@ -83,7 +78,7 @@ delete_stacks() {
 # 動作確認のための関数
 # ----------------------------------------
 test_stacks() {
-  API_GATEWAY_URL=$(aws cloudformation describe-stacks --stack-name ParentStack --query "Stacks[0].Outputs[?OutputKey=='ApiGatewayUrlProd'].OutputValue" --output text)
+  API_GATEWAY_URL=$(aws cloudformation describe-stacks --stack-name $PARENT_STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='ApiGatewayUrlProd'].OutputValue" --output text)
   echo "Testing API Gateway URL (Prod): $API_GATEWAY_URL"
 
   if [ -z "$API_GATEWAY_URL" ]; then
